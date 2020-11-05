d7dde0908d4f390e8e8738901fabac8074612880
= <sayeedofficial6220@gmail.com>
2020-11-05 21:51:57 +0530

    Added Form Prototype UI to Right Grid

--- src/features/event/eventdashboard/eventdashboard.jsx
+++ src/features/event/eventdashboard/eventdashboard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Grid } from "semantic-ui-react";
+import { Grid,Button } from "semantic-ui-react";
+import Eventform from "../eventform/eventform";
 import Eventlist from "./evenlist/eventlist";
 
 class EventDashboard extends Component {
@@ -9,7 +10,10 @@ class EventDashboard extends Component {
         <Grid.Column width={10}>
           <Eventlist />
         </Grid.Column>
-        <Grid.Column width={6}></Grid.Column>
+        <Grid.Column width={6}>
+          <Button negative icon="play circle" content="Create Event"/>
+          <Eventform/>
+        </Grid.Column>
       </Grid>
     );
   }
