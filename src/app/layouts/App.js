8ffafd2c9f25dec6b8f6a40b7355870a669795af
= <sayeedofficial6220@gmail.com>
2020-11-19 11:11:39 +0530

    Added Routing functionality to all nav components, Created CMS system on signin and signup and added routing to user signin dash board with conditional rendering

--- src/app/layouts/App.js
+++ src/app/layouts/App.js
@@ -1,16 +1,37 @@
 import React, { Component, Fragment } from "react";
+import { Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/event/eventdashboard/eventdashboard";
-import Navbar from "../../features/event/eventdashboard/nav/navbar/navbar";
+import { eventdetailedpage } from "../../features/event/eventdetailedpage/eventdetailedpage";
+import Eventform from "../../features/event/eventform/eventform";
+import { homepage } from "../../features/home/homepage";
+import Navbar from "../../features/navbar/navbar";
+import { peopledashboard } from "../../features/user/PeopleDashboard/peopledashboard";
+import { Settingsdashboard } from "../../features/user/settings/settingsdashboard";
 
+import { userdetailed } from "../../features/user/userdetailed/userdetailed";
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Container className="main">
-          <Navbar />
-          <EventDashboard />
-        </Container>
+        <Route exact path="/" component={homepage} />
+        <Route
+          path="/(.+)"
+          render={() => (
+            <Fragment>
+              <Navbar />
+              <Container className="main">
+                <Route exact path="/" component={homepage} />
+                <Route path="/events" component={EventDashboard} />
+                <Route path="/events/:id" component={eventdetailedpage} />
+                <Route path="/people" component={peopledashboard} />
+                <Route path="/profile/:id" component={userdetailed} />
+                <Route path="/settings" component={Settingsdashboard} />
+                <Route path="/createEvent" component={Eventform} />
+              </Container>
+            </Fragment>
+          )}
+        />
       </Fragment>
     );
   }
---
+++ src/features/event/eventdetailedpage/eventdetailedpage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const eventdetailedpage = () => {
+    return (
+        <div>
+            <h1>Event Detailed </h1>
+        </div>
+    )
+}
