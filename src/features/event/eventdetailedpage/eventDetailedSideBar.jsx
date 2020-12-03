61db76b72ed662423fce772b531572553d56cacf
sayeedofficial <56059227+sayeedofficial@users.noreply.github.com>
2020-12-03 20:49:44 +0530

    Added Static Prototype of header,side bar and chat Design for event detailed page

--- src/features/event/eventdetailedpage/eventDetailedSideBar.jsx
+++ src/features/event/eventdetailedpage/eventDetailedSideBar.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Item, Label, List, Segment } from "semantic-ui-react";
 
 const EventDetailedSideBar = () => {
   return (
-    <div>
-      <h1>EventDetailedSideBar</h1>
-    </div>
+    <Fragment>
+      <Segment
+        textAlign="center"
+        style={{ border: "none" }}
+        attached="top"
+        secondary
+        inverted
+        color="teal"
+      >
+        2 People Going
+      </Segment>
+      <Segment attached>
+        <List relaxed divided>
+          <Item style={{ position: "relative" }}>
+            <Label
+              style={{ position: "absolute" }}
+              color="orange"
+              ribbon="right"
+            >
+              Host
+            </Label>
+            <Item.Image size="tiny" src="/assets/user.png" />
+            <Item.Content verticalAlign="middle">
+              <Item.Header as="h3">
+                <a>Attendee Name</a>
+              </Item.Header>
+            </Item.Content>
+          </Item>
+        </List>
+      </Segment>
+    </Fragment>
   );
 };
 
