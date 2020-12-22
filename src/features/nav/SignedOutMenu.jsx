035903356f9ea16fecd4d2a2e3a61fba9da2777d
sayeedofficial <56059227+sayeedofficial@users.noreply.github.com>
2020-12-23 00:39:42 +0530

    Added Register User Funtionality

--- src/features/nav/SignedOutMenu.jsx
+++ src/features/nav/SignedOutMenu.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import { Menu, Button } from 'semantic-ui-react';
-import { useDispatch } from 'react-redux';
-import { openModal } from '../../app/common/modals/modalReducer';
+import React from "react";
+import { Menu, Button } from "semantic-ui-react";
+import { useDispatch } from "react-redux";
+import { openModal } from "../../app/common/modals/modalReducer";
 
-export default function SignedOutMenu({setAuthenticated}) {
+export default function SignedOutMenu({ setAuthenticated }) {
   const dispatch = useDispatch();
   return (
-    <Menu.Item position='right'>
-      <Button onClick={() => dispatch(openModal({modalType: 'LoginForm'}))} basic inverted content='Login' />
+    <Menu.Item position="right">
       <Button
+        onClick={() => dispatch(openModal({ modalType: "LoginForm" }))}
         basic
         inverted
-        content='Register'
-        style={{ marginLeft: '0.5em' }}
+        content="Login"
+      />
+      <Button
+        onClick={() => dispatch(openModal({ modalType: "RegisterForm" }))}
+        basic
+        inverted
+        content="Login"
+        basic
+        inverted
+        content="Register"
+        style={{ marginLeft: "0.5em" }}
       />
     </Menu.Item>
   );
