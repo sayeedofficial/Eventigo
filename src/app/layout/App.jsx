c32f7e557c48c192a4540db03f2ad43f48e49f57
sayeedofficial <56059227+sayeedofficial@users.noreply.github.com>
2020-12-30 13:29:53 +0530

    Rebased to commit id 50d7716d4f0cabbe14d33ab8a7400f0f6f7aecdd

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -13,7 +13,6 @@ import ErrorComponent from '../common/errors/ErrorComponent';
 import AccountPage from '../../features/auth/AccountPage';
 import { useSelector } from 'react-redux';
 import LoadingComponent from './LoadingComponent';
-import ProfilePage from '../../features/profiles/profilePage/ProfilePage';
 
 export default function App() {
   const { key } = useLocation();
@@ -41,7 +40,6 @@ export default function App() {
                 key={key}
               />
               <Route path='/account' component={AccountPage} />
-              <Route path='/profile/:id' component={ProfilePage} />
               <Route path='/error' component={ErrorComponent} />
             </Container>
           </>
--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -4,15 +4,13 @@ import eventReducer from '../../features/events/eventReducer';
 import modalReducer from '../common/modals/modalReducer';
 import authReducer from '../../features/auth/authReducer';
 import asyncReducer from '../async/asyncReducer';
-import profileReducer from '../../features/profiles/profileReducer';
 
 const rootReducer = combineReducers({
     test: testReducer,
     event: eventReducer,
     modals: modalReducer,
     auth: authReducer,
-    async: asyncReducer,
-    profile: profileReducer
+    async: asyncReducer
 })
 
 export default rootReducer;
